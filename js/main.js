d281e3b9928e0ccf3d1a3c99689ca947ebd64174
katriel Araujo <katrielaraujo@gmail.com>
2020-12-27 15:12:31 -0300

    Aula01 terminada

---
+++ js/main.js
@@ -0,0 +1,55 @@
+
+
+/*
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+
+var count;
+for(count = 0; count <= 5; count++){
+    alert(count);
+}
+
+
+var count = 0;
+while(count <= 5){
+    alert(count++);
+};
+
+var idade = prompt("Qual sua idade: ");
+if (idade >= 18){
+    alert("maior de idade");
+}else {
+    alert("menor de idade")
+};
+
+
+
+var frutas = [{nome:"maça",cor:"vermelha"},{nome:"uva",cor:"roxa"}];
+console.log(frutas);
+alert(frutas[1].nome);
+
+var fruta = {nome:"maça",cor:"vermelha"};
+console.log(fruta.nome);
+alert(fruta.cor)
+
+var lista = ["maça","pera","laranja"];
+lista.push("uva");
+//lista.pop();
+
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" - "));
+
+
+var nome = "Katriel Araújo";
+var n1 = 5;
+var n2 = 3;
+var frase = "Japão é o melhor time do mundo";
+//alert(nome+" tem "+idade+" anos");
+//alert(idade+idade2);
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.toLowerCase());
+*/
